1ddef4f7c0484fecfd974de1e7a93d50a8dc22f0
Azhman <bapir.dev@gmail.com>
2022-02-16 20:55:05 +0330

    SetupColors is done

---
+++ Calculator/Utils/Enum/StyleGuide.swift
@@ -0,0 +1,58 @@
+//
+//  StyleGuide.swift
+//  Calculator
+//
+//  Created by Amirreza Zarepour on 2/16/22.
+//
+
+import SwiftUI
+
+enum StyleGuide{
+    
+    enum Colors{
+        enum Primary{
+            static let red    = Color("primary1")
+            static let light  = Color("primary2")
+            static let dark   = Color("primary3")
+            static let pink   = Color("primary4")
+            static let purple = Color("primary5")
+            static let green  = Color("primary6")
+            static let blue   = Color("primary7")
+        }
+        enum Secondry{
+            static let red    = Color("secondry1")
+            static let light  = Color("secondry2")
+            static let dark   = Color("secondry3")
+            static let pink   = Color("secondry4")
+            static let purple = Color("secondry5")
+            static let green  = Color("secondry6")
+            static let blue   = Color("secondry7")
+        }
+        enum PrimaryText{
+            static let red    = Color("primaryText1")
+            static let light  = Color("primaryText")
+            static let dark   = Color("primaryText3")
+            static let pink   = Color("primaryText4")
+            static let purple = Color("primaryText5")
+            static let green  = Color("primaryText6")
+            static let blue   = Color("primaryText7")
+        }
+        enum SecondryText{
+            static let red    = Color("secondryText1")
+            static let light  = Color("secondryText2")
+            static let dark   = Color("secondryText3")
+            static let pink   = Color("secondryText4")
+            static let purple = Color("secondryText5")
+            static let green  = Color("secondryText6")
+            static let blue   = Color("secondryText7")
+        }
+        enum DisplayText{
+            static let red    = Color("displayText1")
+            static let dark   = Color("displayText3")
+        }
+    }
+    
+}
+
+
+
---
+++ Calculator/Utils/Extension/Color.swift
@@ -0,0 +1,12 @@
+//
+//  Color.swift
+//  Calculator
+//
+//  Created by Amirreza Zarepour on 2/16/22.
+//
+
+import SwiftUI
+
+extension Color{
+    static let themes = StyleGuide.Colors.self
+}
